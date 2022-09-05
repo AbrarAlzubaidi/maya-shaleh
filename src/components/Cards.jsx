6d2aaaae04860adbdd83b7e76abe45426c7ff407
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2022-09-06 01:53:03 +0300

    80% finished

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,11 +3,12 @@ import "../assets/css/Cards.css";
 
 export default function Cards(props){
     // console.log(props.item.title)
+    let image_path = "../src/assets/images/"+props.item.image;
     return(
         <div className="features">
             <div className="card">
                 <div className="image">
-                    <img src={`../src/assets/images/${props.item.image}`}/>
+                    <img src={image_path}/>
                 </div>
                 <div className="card-content">
                     <h2 className="card-title">{props.item.title}</h2>
